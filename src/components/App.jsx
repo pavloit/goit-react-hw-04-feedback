6dc873d18fe7861bd538bb7b5a027106097c25e0
pavloit <dba.devop@gmail.com>
2023-12-31 12:33:15 +0200

    feedback hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,52 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Feedback from './feedback/FeedBack'
 import Statistics from "./statistics/Statistics";
 import Section from './section/Section'
 import Notification from './notification/Notification'
 
+export const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  function handleFeedback(type) {
+    if (type === 'good') return setGood((prev) => prev +=1)
+    if (type === 'neutral') return setNeutral((prev) => prev +=1)
+    if (type === 'bad') return setBad((prev) => prev +=1)
+    }
 
-export class App extends Component {
-  state = {
-      good: 0,
-      neutral: 0,
-      bad: 0
+  const countTotalFeedback = () => good + neutral + bad;
+  const countPositiveFeedbackPercentage = () => {
+    const percentage = (good / countTotalFeedback() * 100)
+    return percentage
   };
   
-  handleFeedback = (type) => {
-    this.setState((prevState) => ({
-      [type]: prevState[type] + 1
-    }));
+  const centerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh'
   }
 
-  countTotalFeedback = () => this.state.good + this.state.neutral + this.state.bad;
-  
-  countPositiveFeedbackPercentage = () => { 
-    const percentage = (this.state.good / this.countTotalFeedback() * 100)
-    return percentage
-  } ;
-
-
-  render() {
-   const centerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh'
-    };
-
-    return (
-        <div style={centerStyle}>
-            <Section title="Please leave feedback">
-                <Feedback options={['good', 'neutral', 'bad']} onLeaveFeedback={this.handleFeedback} />
-            </Section>
-            <Section title="Statistics">
-                {this.countTotalFeedback() > 0 ? (
-                    <Statistics
-                        good={this.state.good}
-                        neutral={this.state.neutral}
-                        bad={this.state.bad}
-                        total={this.countTotalFeedback()}
-                        positivePercentage={this.countPositiveFeedbackPercentage()}
-                    />
-                ) : (
-                    <Notification message="There is no feedback" />
-                )}
-            </Section>
-      </div>
-    );
-  }
+  return (   
+  <div style={centerStyle}>
+    <Section title="Please leave feedback">
+      <Feedback options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback} />
+    </Section>
+    <Section title="Statistics">
+      {countTotalFeedback() > 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </Section>
+    </div>
+  )
 }
